aae489d60315c5863463ef9819218915795dfd43
kaisei0528 <k.shimayama.23.6.1091@4nonome.com>
2023-07-12 17:36:59 +0900

    制御構文の解答

--- trapezoidal_integral.py
+++ trapezoidal_integral.py
@@ -3,3 +3,9 @@ from math import sin
 # print(sin(0))
 # >>> 0
 # -----------
+h = (pi / 2) / 100
+total = 0
+for k in range(1, 101):
+    s = (h / 2)*((sin(k - 1) * h) + (sin(k * h)))
+    total += s
+print(total)
