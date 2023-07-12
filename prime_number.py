01498fc37b308c15852e0b4ee2fdea7f8cfa50df
kaisei0528 <k.shimayama.23.6.1091@4nonome.com>
2023-07-12 17:44:43 +0900

    制御構文の解答

--- prime_number.py
+++ prime_number.py
@@ -1,4 +1,31 @@
-a = input("aの値を入力: ")
-b = input("bの値を入力: ")
+number1 = 61
+number2= 10
 
 # TODO
+def is_prime(n):
+    if n <= 1:
+        return False
+    elif n <= 3:
+        return True
+    elif n % 2 == 0 or n % 3 == 0:
+        return False
+    i = 5
+    while i * i <= n:
+        if n % i == 0 or n % (i + 2) == 0:
+            return False
+        i += 6
+    return True    
+
+if is_prime(number1):
+    print(number1,"は素数です")
+else:
+    print(number1,"は素数ではありません")    
+if is_prime(number2):
+    print(number2,"は素数です")
+else:
+    print(number2,"は素数ではありません")                 
+
+
+
+
+    
