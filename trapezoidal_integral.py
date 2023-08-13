f36141ec74d29b051e5e7ada59c900bf8403b77c
kaisei0528 <k.shimayama.23.6.1091@4nonome.com>
2023-08-13 16:14:57 +0900

    trapezoidal

--- trapezoidal_integral.py
+++ trapezoidal_integral.py
@@ -1,22 +1,46 @@
 import math
 
-# --example--
-# print(sin(0))
-# >>> 0
-# -----------
+
+def trapezoidal_integration(f, a=0,b=1,n=100):
+    h = (b - a) / n
+    integral_sum = 0
+   
+    for i in range(1,n + 1):
+           x1 = a + (i - 1) * h
+           x2 = a + i * h
+           integral_sum += (f(x1) + f(x2)) / 2
+        
+    integral_value = h * integral_sum
+    return integral_value
+
+#(1)
 a = 0
-b = math.pi/2
+b=(math.pi)/2
+n=50
+f1 = math.sin
+a1 = trapezoidal_integration(f1,a,b,n)
+print(a1)
+
+
+#(2)
+a = 0
+b = 1
 n = 100
+def f2(x):
+      return 4 / (1 + (x**2))
+a2 = trapezoidal_integration(f2,a,b,n)
+print(a2)
 
 
+#(3)
+a = -100
+b = 100
+n = 1000
+def f3(x):
+      return ((math.pi)**(1/2)) * (math.e**(-x**2))
+a3 = trapezoidal_integration(f3,a,b,n)
+print(a3)
 
+           
+           
 
-
-
-h = b / n
-total = 0
-for k in range(1, n + 1):
-    s = (h / 2)*((math.sin((k - 1) * h)) + (math.sin(k * h)))
-    total += s
-print(total)
-print(type(total))
