135f1e85fce75a966d610dbe93e6aef3b189c9f3
kaisei0528 <k.shimayama.23.6.1091@4nonome.com>
2023-08-15 21:22:27 +0900

    問３の解答

--- euclid.py
+++ euclid.py
@@ -1,11 +1,4 @@
-
-
-
-# TODO
-
-# TODO
-
-
+#TO do
 def gcd(a,b):
     while b != 0:
         a, b = b, a % b
@@ -13,22 +6,3 @@ def gcd(a,b):
 
 
 
-#(1)
-a = 10
-b = 20
-
-result = gcd(a, b)
-print("最大公約数:", result)
-#(2)
-c = 14
-d = 91
-
-result = gcd(c, d)
-print("最大公約数:", result)
-
-#(3)
-e = 91
-f = 14
-
-result = gcd(e, f)
-print("最大公約数:", result)
