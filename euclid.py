5f47a3b5af5c4d989ffa3c550b413bf578c84933
kaisei0528 <k.shimayama.23.6.1091@4nonome.com>
2023-09-03 20:11:19 +0900

    answer3,4

--- euclid.py
+++ euclid.py
@@ -3,6 +3,17 @@ def gcd(a,b):
     while b != 0:
         a, b = b, a % b
     return a
+def judge_prime(a,b):
+    if gcd(a,b) == 1:
+        return True
+    else:
+        return False
+judge_prime(5,3)        
+
+     
+
+          
+
 
 
 
