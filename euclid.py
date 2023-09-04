d2b425e4caf0626787d6f930a586824cba4d1abe
kaisei0528 <k.shimayama.23.6.1091@4nonome.com>
2023-09-04 21:25:06 +0900

    answer3.4

--- euclid.py
+++ euclid.py
@@ -4,11 +4,15 @@ def gcd(a,b):
         a, b = b, a % b
     return a
 def judge_prime(a,b):
+    gcd_result = gcd(a,b)
     if gcd(a,b) == 1:
         return True
     else:
         return False
-judge_prime(5,3)        
+a = 2
+b = 3
+result = judge_prime(a,b)
+print(result)
 
      
 
