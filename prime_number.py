e1d54b9b4a8a4765cec9842f931c837307a3e7ee
kaisei0528 <k.shimayama.23.6.1091@4nonome.com>
2023-09-06 11:08:06 +0900

    素数判定

--- prime_number.py
+++ prime_number.py
@@ -2,19 +2,9 @@ number1 = 61
 number2= 10
 
 # TODO
+
 def is_prime(n):
-    if n <= 1:
-        return False
-    elif n <= 3:
-        return True
-    elif n % 2 == 0 or n % 3 == 0:
-        return False
-    i = 5
-    while i * i <= n:
-        if n % i == 0 or n % (i + 2) == 0:
-            return False
-        i += 6
-    return True
+    return n > 1 and all(n % i != 0 for i in range(2,int(n**0.5) + 1))
     
 #素数判定
 if is_prime(number1):
